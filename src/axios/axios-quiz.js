8898465de3c32595f0a6d4df8225a33ac2755979
Anton Yermakovich <antonyermakovich@gmail.com>
2021-10-12 19:49:12 +0300

    initial

---
+++ src/axios/axios-quiz.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL: 'https://create-quiz-95333-default-rtdb.firebaseio.com'
+})
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import {createStore, compose, applyMiddleware} from 'redux'
+import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import rootReducers from "./store/reducers/rootReducers"
+import thunk from "redux-thunk";
+
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        }) : compose
+
+const store = createStore(
+    rootReducers,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
